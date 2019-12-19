2fa79fd86d699381ce740f1fdc52880dc3763112
Erisa <seriel@erisa.moe>
2019-12-19 15:07:50 +0000

    Sanitise colons in filenames For #6

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-mimeTypes = {"image/aces":["exr"],"image/apng":["apng"],"image/bmp":["bmp"],"image/cgm":["cgm"],"image/dicom-rle":["drle"],"image/emf":["emf"],"image/fits":["fits"],"image/g3fax":["g3"],"image/gif":["gif"],"image/heic":["heic"],"image/heic-sequence":["heics"],"image/heif":["heif"],"image/heif-sequence":["heifs"],"image/ief":["ief"],"image/jls":["jls"],"image/jp2":["jp2","jpg2"],"image/jpeg":["jpeg","jpg","jpe"],"image/jpm":["jpm"],"image/jpx":["jpx","jpf"],"image/jxr":["jxr"],"image/ktx":["ktx"],"image/png":["png"],"image/sgi":["sgi"],"image/svg+xml":["svg","svgz"],"image/t38":["t38"],"image/tiff":["tif","tiff"],"image/tiff-fx":["tfx"],"image/webp":["webp"],"image/wmf":["wmf"]}
+mimeTypes = { "image/aces": ["exr"], "image/apng": ["apng"], "image/bmp": ["bmp"], "image/cgm": ["cgm"], "image/dicom-rle": ["drle"], "image/emf": ["emf"], "image/fits": ["fits"], "image/g3fax": ["g3"], "image/gif": ["gif"], "image/heic": ["heic"], "image/heic-sequence": ["heics"], "image/heif": ["heif"], "image/heif-sequence": ["heifs"], "image/ief": ["ief"], "image/jls": ["jls"], "image/jp2": ["jp2", "jpg2"], "image/jpeg": ["jpeg", "jpg", "jpe"], "image/jpm": ["jpm"], "image/jpx": ["jpx", "jpf"], "image/jxr": ["jxr"], "image/ktx": ["ktx"], "image/png": ["png"], "image/sgi": ["sgi"], "image/svg+xml": ["svg", "svgz"], "image/t38": ["t38"], "image/tiff": ["tif", "tiff"], "image/tiff-fx": ["tfx"], "image/webp": ["webp"], "image/wmf": ["wmf"] }
 
 function onError(error) {
   console.log(`Error: ${error}`);
@@ -29,7 +29,7 @@ function doDownload(url, prefix) {
     let base64ContentArray = url.split(",");
     let mimeType = base64ContentArray[0].match(/[^:\s*]\w+\/[\w-+\d.]+(?=[;| ])/)[0];
     ext = mimeTypes[mimeType][0];
-    var file = base64ContentArray[1].substring(0,16).replace("/", "0");
+    var file = base64ContentArray[1].substring(0, 16).replace("/", "0");
     targetFilename = prefix + file + "." + ext;
 
     byteString = atob(base64ContentArray[1]);
@@ -37,10 +37,10 @@ function doDownload(url, prefix) {
     var ia = new Uint8Array(byteString.length);
 
     for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
+      ia[i] = byteString.charCodeAt(i);
     }
 
-    let blob = URL.createObjectURL(new Blob([ia], {type: mimeType}));
+    let blob = URL.createObjectURL(new Blob([ia], { type: mimeType }));
 
     chrome.downloads.download({
       url: blob,
@@ -48,7 +48,7 @@ function doDownload(url, prefix) {
       filename: targetFilename
     });
   } else {
-    var file = url.split('/').pop().split('#')[0].split('?')[0];
+    var file = url.split('/').pop().split('#')[0].split('?')[0].replace(':', '_');
     targetFilename = prefix + file;
     chrome.downloads.download({
       url: url,
@@ -56,6 +56,6 @@ function doDownload(url, prefix) {
       filename: targetFilename
     });
   }
-  
-  
+
+
 }
